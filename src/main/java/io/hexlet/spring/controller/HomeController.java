11cffebbf6d85b81f01271fec3b25c48be0763e8
Julia Kon <kon1976y@gmail.com>
2025-09-27 17:28:42 +0300

    add application.yml

--- src/main/java/io/hexlet/spring/controller/HomeController.java
+++ src/main/java/io/hexlet/spring/controller/HomeController.java
@@ -1,10 +1,13 @@
 package io.hexlet.spring.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;;
 
 @RestController
 public class HomeController {
+    @Value("${app.welcome-message}")
+    private String welcomeMessage;
     @GetMapping("/")
     public String home() {
         return "Добро пожаловать в Hexlet Spring Blog!";
@@ -13,5 +16,10 @@ public class HomeController {
     @GetMapping("/about")
     public String about() {
         return "Это простой блог на Spring. Я только учусь";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return welcomeMessage;
     }
 }
